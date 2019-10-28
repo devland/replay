e3c9ec899738fa2b390a8382b379aa906cc94515
devland <5208532+devland@users.noreply.github.com>
2019-10-28 21:14:12 +0200

    load js scripts through proxy;

--- extra.js
+++ extra.js
@@ -38,6 +38,7 @@ function extra()
  this.cookies={
   parse: (input)=>
   {
+   if (!input) return {};
    let list=input.split(';');
    let output={};
    for (item of list)
@@ -53,6 +54,64 @@ function extra()
    for (key in cookies) list.push(`${key}=${cookies[key]}`);
    return list.join(';');
   }
+ }
+ this.bindProperties=function(firstObject, firstProperty, secondObject, secondProperty)
+ {
+  if (typeof firstObject!="object" || typeof secondObject!="object" ||
+      typeof firstProperty!="string" || typeof secondProperty!="string")
+  {
+   console.log("invalid input for bindProperties method");
+   return;
+  }
+  var getProtoDesc=function(object, property)
+  {
+   var output=
+   {
+    prototype: object,
+    descriptor: null
+   };
+   output.descriptor=Object.getOwnPropertyDescriptor(output.prototype, property);
+   if (output.descriptor) return output;
+   else return getProtoDesc(Object.getPrototypeOf(output.prototype), property);
+  }
+  var bind=function(protoDesc, oldProtoDesc, property, otherProtoDesc, otherObject, otherProperty)
+  {
+   if (protoDesc.descriptor.writable)
+   {
+    delete protoDesc.descriptor.writable;
+    protoDesc.ownValue=protoDesc.descriptor.value;
+    delete protoDesc.descriptor.value;
+    protoDesc.descriptor.get=function()
+    {
+     return protoDesc.ownValue;
+    }
+   }
+   protoDesc.descriptor.configurable=true;
+   protoDesc.descriptor.set=function()
+   {
+    console.log(arguments[0]);
+    if (typeof protoDesc.ownValue!="undefined") protoDesc.ownValue=arguments[0];
+    if (!protoDesc.noBind)
+    {
+     otherProtoDesc.noBind=1;
+     otherObject[otherProperty]=arguments[0];
+    }
+    else protoDesc.noBind=0;
+    if (typeof oldProtoDesc.descriptor.set=="function") oldProtoDesc.descriptor.set.apply(this, arguments);
+   }
+   Object.defineProperty(protoDesc.prototype, property, protoDesc.descriptor);
+  }
+  var firstOldProtoDesc=getProtoDesc(firstObject, firstProperty),
+      secondOldProtoDesc=getProtoDesc(secondObject, secondProperty),
+      firstProtoDesc=getProtoDesc(firstObject, firstProperty),
+      secondProtoDesc=getProtoDesc(secondObject, secondProperty);
+  console.log(firstProtoDesc.descriptor.set);
+  console.log(secondProtoDesc.descriptor.set);
+  bind(firstProtoDesc, firstOldProtoDesc, firstProperty, secondProtoDesc, secondObject, secondProperty);
+  bind(secondProtoDesc, secondOldProtoDesc, secondProperty, firstProtoDesc, firstObject, firstProperty);
+  console.log(firstProtoDesc.descriptor.set);
+  console.log(secondProtoDesc.descriptor.set);
+  console.log("cheese");
  }
 }
 if (typeof module!="undefined" && module.exports) module.exports=new extra();
--- proxy.js
+++ proxy.js
@@ -1,183 +1,71 @@
-const protocols=
- {
-  "http:": require("http"),
-  "https:": require("https")
- },
- port=process.argv[2] || 8080,
- url=require("url"),
- util=require("util"),
- fs=require("fs"),
- extra=require("./extra.js");
- let targetUrl=new URL("http://example.com");
- let proxyUrl=new URL(`http://127.0.0.1:${port}/`);
- let staticFiles=["replay.html", "replay.js", "extra.js"];
- let proxyTarget={};
-const log=function()
+<html>
+ <head>
+  <script type="text/javascript" src="extra.js"></script>
+  <script type="text/javascript">
+let xtra;
+let proxyUrl=new URL("http://127.0.0.1:8080/");
+let target, frame, framePath;
+let cookies={};
+const tunnel=(url)=>
 {
- var now=new Date(), line="["+now+" ."+now.getMilliseconds()+"]: ";
- for (let i in arguments)
-  if (typeof arguments[i] == "string") line+=arguments[i];
-  else
-  {
-   console.log(line);
-   line="";
-   console.log(util.inspect(arguments[i], false, null, true));
-  }
-  if (line) console.log(line);
+ if (!url) return "";
+ if (url.includes(proxyUrl.href)) url=url.substring(proxyUrl.href.length);
+ let targetUrl;
+ try
+ {
+  targetUrl=new URL(url);
+ }
+ catch(error)
+ {
+  targetUrl=new URL(url, framePath.origin);
+ }
+ url=targetUrl.href;
+ return proxyUrl.href+url;
 }
-const proxy=(request, response)=>
+const loadTarget=(url)=>
 {
- let cookies=extra.cookies.parse(request.headers.cookie);
- if (cookies.proxyTargetHref) proxyTarget=url.parse(cookies.proxyTargetHref);
- let targetUrl=url.parse(request.url.substring(1), true);
- if (proxyTarget.host!=targetUrl.host)
-  targetUrl=url.parse(`${proxyTarget.protocol}//${proxyTarget.host}/${targetUrl.href.replace(/https?:\/\//, "")}`);
- if (cookies.proxyTargetHref)
+ if (url)
  {
-  delete cookies.proxyTargetHref;
-  request.headers.cookie=extra.cookies.stringify(cookies);
+  url=tunnel(url);
+  let destination=url.includes(proxyUrl.href)?url.substring(proxyUrl.href.length):url;
+  target.value=destination;
  }
- let targetUrlOptions=
- {
-  host: targetUrl.host,
-  port: targetUrl.protocol==="https:"?443:80,
-  path: targetUrl.path,
-  method: request.method,
-  headers: request.headers,
-  timeout: 10000
- }
- if (targetUrlOptions.headers)
- {
-  delete targetUrlOptions.headers.referer;
-  delete targetUrlOptions.headers.host;
- }
- if (!targetUrl) targetUrl={href: null};
- log(`proxying request to "${targetUrl.href}"`);
- if (protocols[targetUrl.protocol])
- {
-  const proxyRequest=protocols[targetUrl.protocol].request(targetUrlOptions, (result)=>
-  {
-   delete result.headers["x-frame-options"];
-   if (Array.isArray(result.headers["set-cookie"]))
-    for (let i=0; i<result.headers["set-cookie"].length; i++)
-    {
-     result.headers["set-cookie"][i]=result.headers["set-cookie"][i].replace(/domain=(.*?)((; )|$)/, "");
-     result.headers["set-cookie"][i]=result.headers["set-cookie"][i].replace(/ secure((; )|$)/, "");
-    }
-   log(`response headers for "${targetUrl.href}"`, result.headers);
-   if (result.headers.location)
-   {
-    log(`redirecting to ${result.headers.location}`);
-    const location=url.parse(result.headers.location);
-    if (!location.host)
-    {
-     let newLocation=url.resolve(targetUrl.href, result.headers.location);
-     result.headers.location=proxyUrl.href+newLocation;
-     proxyTarget=url.parse(newLocation);
-    }
-    else
-    {
-     proxyTarget=url.parse(result.headers.location);
-     result.headers.location=proxyUrl.href+result.headers.location;
-    }
-    if (!Array.isArray(result.headers["set-cookie"])) result.headers["set-cookie"]=[];
-    result.headers["set-cookie"].push(`proxyTargetHref=${proxyTarget.href}; path=/`);
-    response.writeHead(result.statusCode, result.headers);
-    result.pipe(response);
-   }
-   else
-   {
-    result.headers["Access-Control-Allow-Origin"]="*";
-    log(`proxy request response status code for "${targetUrl.href}": ${result.statusCode}`);
-    response.writeHead(result.statusCode, result.headers);
-    result.pipe(response);
-   }
-  });
-  proxyRequest.on("error", (error)=>
-  {
-   log(`proxy request error for ${targetUrl.href}`, error);
-   response.writeHead(500);
-   response.end();
-  });
-  return proxyRequest;
- }
- else
- {
-  log(`no protocol for ${targetUrl.href}`);
-  response.writeHead(500);
-  response.end();
-  return null;
- }
+ else url=tunnel(target.value);
+ document.cookie=`proxyTargetHref=${target.value}; path=/`;
+ frame.src=url;
 }
-process.on("error", (error)=>
+window.addEventListener("load", (event)=>
 {
- log(`[error] process error`, error);
+ target=document.getElementById("targetHref");
+ frame=document.getElementById("frame");
+ frame.addEventListener("load", (event)=>
+ {
+  if (!frame.contentDocument) return null;
+  console.log(`loaded ${frame.contentDocument.location.href}`);
+  framePath=new URL(frame.contentWindow.location.href.substring(proxyUrl.href.length));
+  frame.contentDocument.addEventListener("click", (event)=>
+  {
+   console.log("click");
+   let link=event.target.closest("a");
+   let form=event.target.closest("form");
+   if (link && link.href)
+   {
+    console.log(link.href);
+    loadTarget(link.href);
+    event.preventDefault();
+   }
+   else console.log("no link...");
+   if (form) form.action=tunnel(form.getAttribute("action"));
+  });
+ });
 });
-const requestHandler=(request, response)=>
-{
- log("request headers:", request.headers);
- let path=request.url;
- request.on("error", (error)=>
- {
-  log("[error]", error);
-  response.end(error);
- });
- let fileName="";
- extra.cascade(
- [
-  (next)=>
-  {
-   var parts=path.split("/");
-   for (var i=0; i<parts.length; i++) if (!parts[i]) parts.splice(i, 1);
-   fileName=parts.join("/");
-   next(null);
-  },
-  (next)=>
-  {
-   if (!staticFiles.includes(fileName)) return next("proxy");
-   var benchmarkStart = new Date();
-   fs.readFile(fileName, "binary", (error, result)=>
-   {
-    if (error) next(error);
-    else
-    {
-     next(null, result);
-     var benchmarkEnd = new Date();
-     log("served file "+fileName, (" ("+(benchmarkEnd-benchmarkStart)/1000+" s)"));
-    }
-   });
-  }
- ],
- (error, result)=>
- {
-  if (error)
-  {
-   if (error=="proxy")
-   {
-    const targetUrl=url.parse(request.url.substring(1), true);
-    let stream=proxy(request, response);
-    if (stream) request.pipe(stream);
-    else
-    {
-     log(`no stream for ${targetUrl.href}`);
-     response.writeHead(500);
-     response.end();
-    }
-   }
-   else
-   {
-    log("error", error);
-    response.write(error + "\n");
-    response.end();
-   }
-  }
-  else
-  {
-   response.writeHead(200);
-   response.write(result, "binary");
-   response.end();
-  }
- });
-}
-protocols["http:"].createServer(requestHandler).listen(parseInt(port, 10));
-log("static file server running at http://localhost:"+port);
+  </script>
+ </head>
+ <body>
+  <input type="text" id="targetHref" placeholder="website url" value="https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onunload">
+  <button type="button" onclick="loadTarget()">Change Content</button>
+  <div>
+   <iframe id="frame" style="width: 100vw; height: 90vh;"></iframe>
+  </div>
+ </body>
+</html>
